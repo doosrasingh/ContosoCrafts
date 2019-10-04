9606ac2e0bbff33145a4e2371f06c2041daeb302
Arun Kumar <arun.kumar1@bayer.com>
2019-10-04 16:59:02 +0200

    Add project files.

---
+++ ContosoCraftsWebsite/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCraftsWebsite.Models;
+using ContosoCraftsWebsite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCraftsWebsite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]//[HttpPost] or [HttpPut] or [HttpPatch] "[FromBody]"
+        public ActionResult Get(
+            [FromQuery] string productId,
+            [FromQuery] int rating)
+        {
+            ProductService.AddRating(productId, rating);
+            return Ok();
+        }
+    }
+}
---
+++ ContosoCraftsWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using ContosoCraftsWebsite.Models;
+using ContosoCraftsWebsite.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCraftsWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService { get; set; }
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
